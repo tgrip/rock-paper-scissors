e422442b462364944fcf217adfe31cca6370429c
Theodor Grip <theo.grip@googlemail.com>
2020-11-20 14:12:17 +0100

    rock-paper-scissors-1: refactor to data driven testing

--- src/test/java/com/example/rockpaperscissors/ShapeTest.groovy
+++ src/test/java/com/example/rockpaperscissors/ShapeTest.groovy
@@ -3,16 +3,24 @@ package com.example.rockpaperscissors
 import spock.lang.Specification
 
 import static com.example.rockpaperscissors.Shape.Paper
+import static com.example.rockpaperscissors.Shape.Paper
 import static com.example.rockpaperscissors.Shape.Rock
 import static com.example.rockpaperscissors.Shape.Scissor
 
 class ShapeTest extends Specification {
 
     def "play with paper"() {
-        when:
+        given:
         def shape = Paper
 
+        when:
+        def gameResult = shape.playAgainst(otherShape)
+
         then:
-        shape.playAgainst(Paper) == GameResult.Draw
+        gameResult == expectedResult
+
+        where:
+        otherShape  | expectedResult
+        Paper       | GameResult.Draw
     }
 }
