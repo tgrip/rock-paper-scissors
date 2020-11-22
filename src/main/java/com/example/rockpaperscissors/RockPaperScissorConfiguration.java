1867cce0408f45011ff65ef467b7de9f74aac87d
Theodor Grip <theo.grip@googlemail.com>
2020-11-22 18:55:52 +0100

    rock-paper-scissors-6 configure game

---
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.rockpaperscissors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class RockPaperScissorConfiguration {
+
+    @Bean
+    public Player player1() {
+        Random random = new Random();
+        PlayerStrategy randomStrategy = new RandomStrategy(random);
+        return new Player(randomStrategy);
+    }
+
+    @Bean
+    public Player player2() {
+        PlayerStrategy sameMoveStrategy = new HandSignStrategy(HandSign.Rock);
+        return new Player(sameMoveStrategy);
+    }
+
+    @Bean
+    public RockPaperScissorGame game() {
+        return new RockPaperScissorGame(player1(), player2(), 100);
+    }
+
+}
---
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorGame.java
@@ -0,0 +1,7 @@
+package com.example.rockpaperscissors;
+
+public class RockPaperScissorGame {
+
+    public RockPaperScissorGame(Player player1, Player player2, int nrRounds) {
+    }
+}
