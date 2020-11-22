10d0839329a1f66a8046e00e94877191335388d0
Theodor Grip <theo.grip@googlemail.com>
2020-11-22 18:25:41 +0100

    rock-paper-scissors-3: manage results for various players

---
+++ src/main/java/com/example/rockpaperscissors/GameStats.java
@@ -0,0 +1,27 @@
+package com.example.rockpaperscissors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameStats {
+
+    private final Map<Player, PlayerStats> statsMap;
+
+    public GameStats() {
+        statsMap = new HashMap<>();
+    }
+
+    public void addResult(Player player, GameResult gameResult) {
+        PlayerStats playerStats = statsMap.computeIfAbsent(player, k -> new PlayerStats());
+        playerStats.addResult(gameResult);
+    }
+
+    public String status(Player player) {
+        PlayerStats stats = statsMap.get(player);
+        if (stats == null) {
+            return "Unknown player";
+        } else {
+            return "Player (" + stats.status() + ")";
+        }
+    }
+}
---
+++ src/test/java/com/example/rockpaperscissors/GameStatsTest.groovy
@@ -0,0 +1,28 @@
+package com.example.rockpaperscissors
+
+import spock.lang.Specification
+
+class GameStatsTest extends Specification {
+
+    def gameStats = new GameStats()
+    def player = new Player(Mock(PlayStrategy))
+
+    def "game stats displays stats for user"() {
+        given:
+        gameStats.addResult(player, GameResult.Draw)
+
+        when:
+        def status = gameStats.status(player)
+
+        then:
+        status == "Player (0 Wins, 1 Draws, 0 Losses)"
+    }
+
+    def "display player unknown"() {
+        when:
+        def status = gameStats.status(player)
+
+        then:
+        status == "Unknown player"
+    }
+}
