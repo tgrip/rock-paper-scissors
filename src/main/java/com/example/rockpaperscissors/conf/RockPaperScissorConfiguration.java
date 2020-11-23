2fa1889d04137a53f17576a9a8a5ee50202c2c67
Theodor Grip <theo.grip@googlemail.com>
2020-11-23 21:03:19 +0100

    refactor visibility

--- src/main/java/com/example/rockpaperscissors/conf/RockPaperScissorConfiguration.java
+++ src/main/java/com/example/rockpaperscissors/conf/RockPaperScissorConfiguration.java
@@ -9,20 +9,20 @@ import java.util.Random;
 @Configuration
 public class RockPaperScissorConfiguration {
 
-    Player player1() {
+    @Bean
+    public RockPaperScissorGame game() {
+        return new RockPaperScissorGame(player1(), player2(), 100);
+    }
+
+    private Player player1() {
         Random random = new Random();
         PlayerStrategy randomStrategy = new RandomStrategy(random);
         return new Player(randomStrategy);
     }
 
-    Player player2() {
+    private Player player2() {
         PlayerStrategy sameMoveStrategy = new HandSignStrategy(HandSign.Rock);
         return new Player(sameMoveStrategy);
-    }
-
-    @Bean
-    public RockPaperScissorGame game() {
-        return new RockPaperScissorGame(player1(), player2(), 100);
     }
 
 }
